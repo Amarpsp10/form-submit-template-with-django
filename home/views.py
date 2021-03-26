a6d5fd29796a13617851c3d4c606f2a144c661bd
amarpsp10 <amarpsp10@gmail.com>
2021-03-26 09:40:35 -0700

    message alert + app registerd

--- home/views.py
+++ home/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render, HttpResponse
+from datetime import datetime
+from home.models import Contact
+from django.contrib import messages
 
 # Create your views here.
 
 
 def index(request):
-    context = {
-        'variable': 'there we go :) '
-    }
-    return render(request, 'myindex.html', context)
-    # return HttpResponse("this is home page")
-
-
-def about(request):
-    return HttpResponse("this is about page")
+    if request.method == "POST":
+        mail = request.POST.get('mail')
+        name = request.POST.get('name')
+        message = request.POST.get('message')
+        contact = Contact(mail=mail, name=name,
+                          message=message, date=datetime.today())
+        contact.save()
+        messages.success(request, 'your message has been sent!')
+    return render(request, 'myindex.html')
